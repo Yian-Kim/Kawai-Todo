987c63bf0d5a83f13caaccdb3294e1684636a30b
Chanmi Kim <chanmi_dev@naver.com>
2019-11-14 21:00:45 +0900

    feat: Add saving ToDos part2
    - prop types 추가
    - ToDo 리스트 저장 기능 추가

--- ToDo.js
+++ ToDo.js
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
-import { View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput } from "react-native";
+import { 
+    View, 
+    Text, 
+    TouchableOpacity, 
+    StyleSheet, 
+    Dimensions, 
+    TextInput
+} from "react-native";
+import PropTypes from "prop-types";
 
 const { width, height } = Dimensions.get("window");
 
 export default class ToDo extends Component{
-    state = {
-        isEditing: false,
-        isCompleted: false,
-        toDoValue: ""
+    constructor(props) {
+        super(props);
+        this.state = { isEditing: false, toDoValue: props.text };
+    }   
+    static propTypes = {
+        text: PropTypes.string.isRequired,
+        isCompleted: PropTypes.bool.isRequired,
+        deleteToDo: PropTypes.func.isRequired,
+        id: PropTypes.string.isRequired
     };
     render(){
         const { isCompleted, isEditing, toDoValue } = this.state;
-        const { text } = this.props;
+        const { text, id, deleteToDo } = this.props;
         return (
             <View style={styles.container}>
                 <View style={styles.column}>
@@ -62,7 +75,7 @@ export default class ToDo extends Component{
                                     <Text style={styles.actions}>✏</Text>
                                 </View>
                             </TouchableOpacity>
-                            <TouchableOpacity>
+                            <TouchableOpacity onPressOut={() => delete(id)}>
                                 <View style={styles.actionContainer}>
                                     <Text style={styles.actions}>❌</Text>
                                 </View>
@@ -81,10 +94,7 @@ export default class ToDo extends Component{
         })
     };
     _startEditing = () => {
-            const { text } = this.props;
-        this.setState({
-            isEditing: true, toDoValue: text
-        });
+        this.setState({ isEditing: true });
     };
     _finishEditing = () => {
         this.setState({
@@ -133,8 +143,7 @@ const styles = StyleSheet.create({
     column: {
         flexDirection: "row",
         alignItems: "center",
-        width: width / 2,
-        justifyContent: "space-between"
+        width: width / 2
     },
     actions: {
         flexDirection: "row"
