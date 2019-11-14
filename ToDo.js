f668c197001185dcb1e0e8a370ad0dec979aabe0
Chanmi Kim <chanmi_dev@naver.com>
2019-11-14 19:17:11 +0900

    feat: Add Todo list buttons
    - 버튼 기능 추가

---
+++ ToDo.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+export default class ToDo extends Component{
+    state = {
+        isEditing: false,
+        isCompleted: false
+    };
+    render(){
+        const { isCompleted, isEditing } = this.state;
+        return (
+            <View style={styles.container}>
+                <View style={styles.column}>
+                    <TouchableOpacity onPress={this._toggleComplete}>
+                        <View 
+                            style={[
+                                styles.circle,
+                                isCompleted ? styles.completedCircle : styles.uncompletedCircle
+                            ]} 
+                        />
+                    </TouchableOpacity>
+                    <Text 
+                        style={[
+                            styles.text,
+                            isCompleted ? styles.completedText : styles.umcompletedText
+                        ]}
+                    >
+                        hello I'm a To Do
+                    </Text>
+                </View>
+                <View style={styles.column}>
+                    {isEditing ? ( 
+                        <View style={styles.actions}>
+                            <TouchableOpacity onPressOut={this._finishEditing}>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actions}>✅</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    ) : (
+                        <View style={styles.actions}>
+                            <TouchableOpacity onPressOut={this._startEditing}>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actions}>✏</Text>
+                                </View>
+                            </TouchableOpacity>
+                            <TouchableOpacity>
+                                <View style={styles.actionContainer}>
+                                    <Text style={styles.actions}>❌</Text>
+                                </View>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
+            </View>
+        );    
+    }
+    _toggleComplete = () => {
+        this.setState(prevState => {
+            return {
+                isCompleted: !prevState.isCompleted
+            };
+        })
+    };
+    _startEditing = () => {
+        this.setState({
+            isEditing: true
+        });
+    };
+    _finishEditing = () => {
+        this.setState({
+            isEditing: false
+        });
+    };
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: width - 50,
+        borderBottomColor: "#bbb",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between"
+    },
+    circle: {
+        width: 30,
+        height: 30,
+        borderRadius: 15,
+        borderWidth: 3,
+        marginRight: 20
+    },
+    completedCircle: {
+        borderColor: "#bbb",
+    },
+    uncompletedCircle: {
+        borderColor: "#F23657",
+    },
+    text: {
+        fontWeight: "600",
+        fontSize: 20,
+        marginVertical: 20
+    },
+    completedText: {
+        color: "#bbb",
+        textDecorationLine: "line-through"
+    },
+    uncompletedText: {
+        color: "#353839"
+    },
+    column: {
+        flexDirection: "row",
+        alignItems: "center",
+        width: width / 2,
+        justifyContent: "space-between"
+    },
+    actions: {
+        flexDirection: "row"
+    },
+    actionContainer: {
+        marginVertical: 10,
+        marginHorizontal: 10
+    }
+});
